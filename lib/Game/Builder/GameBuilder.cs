dff16b4a88b99b0baf3098b3291c9dcb7ccd1cfa
Stepan Maznyi <smazni@gmail.com>
2024-09-08 21:25:35 +0300

    feat: game builder

--- lib/Game/Builder/GameBuilder.cs
+++ lib/Game/Builder/GameBuilder.cs
@@ -1,5 +1,50 @@
+using PaintedPoker.Game.Rules;
+using PaintedPoker.Game.Table;
+
 namespace PaintedPoker.Game.Builder;
 
-public class GameBuilder {
-    
+public class GameBuilder
+{
+    public GameTable<CellMetadata, HeaderMetadata, HeaderMetadata> Build(GameOptions gameOptions)
+    {
+        var headers = Enumerable.Range(0, gameOptions.PlayerCount).Select(index => new HeaderMetadata($"Player {index}"));
+
+        var table = new GameTable<CellMetadata, HeaderMetadata, HeaderMetadata>(headers);
+
+        foreach (var round_size in GenerateRoundSizes(gameOptions.PlayerCount, gameOptions.DeckInfo))
+        {
+            var row = table.NewRow();
+            for (int i = 0; i < row.Count; i++)
+            {
+                row[i] = new CellMetadata(EmptyResult.instance);
+            }
+
+            row.Header = new HeaderMetadata($"Round {round_size}");
+
+            table.Rows.Add(row);
+        }
+        return table;
+    }
+
+    private IEnumerable<int> GenerateRoundSizes(int playerCount, DeckInfo deckInfo)
+    {
+        int max_round_size = GetMaxRoundSize(playerCount, deckInfo);
+
+        for (int i = 1; i <= max_round_size; i++)
+        {
+            yield return i;
+        }
+
+        yield return max_round_size;
+
+        for (int i = max_round_size; i >= 1; i--)
+        {
+            yield return i;
+        }
+    }
+
+    private int GetMaxRoundSize(int playerCount, DeckInfo deckInfo)
+    {
+        return deckInfo.TotalCardsCount / playerCount;
+    }
 }
--- lib/Game/GameConfig.cs
+++ lib/Game/GameConfig.cs
@@ -2,14 +2,30 @@ using PaintedPoker.Game.Services;
 
 namespace PaintedPoker.Game;
 
-public enum DeckSize {
+public enum DeckSize
+{
     Small,
     Large
 }
 
+public interface IDeckSizeInfo
+{
+    public int TotalCardsCount { get; }
+}
+
+
+public class DeckInfo(int valuesCount) : IDeckSizeInfo
+{
+    public static DeckInfo Small = new DeckInfo(9);
+    public static DeckInfo Large = new DeckInfo(12);
+
+    public int TotalCardsCount { get => valuesCount * 4; }
+}
+
 public record GameInfo(GameName GameName, GameOptions GameOptions);
 
-public class GameOptions {
-    public required int PlayerCount { get; set; } 
-    public required DeckSize DeckSize { get; set; } 
+public class GameOptions
+{
+    public required int PlayerCount { get; set; }
+    public required DeckInfo DeckInfo { get; set; }
 }
