17fbf2d367e046478b174ab4936e6405398f9e00
Stepan Maznyi <smazni@gmail.com>
2024-09-08 21:23:31 +0300

    Fix table service types

--- lib/Services/GameTableService.cs
+++ lib/Services/GameTableService.cs
@@ -1,21 +1,26 @@
 using PaintedPoker.Game.Rules;
 using PaintedPoker.Game.Table;
+using TableType = PaintedPoker.Game.Table.GameTable<CellMetadata, HeaderMetadata, HeaderMetadata>;
 
 namespace PaintedPoker.Game.Services;
 
-public record GameName(string name) {
+public record GameName(string name)
+{
     public string FormattedName => name.ToLower().Replace(" ", "-");
 }
 
-public class GameTableService(ILogger<GameTableService> logger) {
-    private Dictionary<string, Table<IGameResult>> _games = new ();
+public class GameTableService(ILogger<GameTableService> logger)
+{
+    private Dictionary<string, TableType> _games = new();
 
-    public void Add(GameName name, Table<IGameResult> table) {
+    public void Add(GameName name, TableType table)
+    {
         _games.Add(name.FormattedName, table);
         logger.LogInformation("New game added, {}. Current count: {}", name.name, _games.Count);
     }
 
-    public Table<IGameResult> GetTable(GameName name) {
+    public TableType GetTable(GameName name)
+    {
         return _games[name.FormattedName];
     }
 }
