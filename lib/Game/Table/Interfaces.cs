d05d147e18f0fc95c5f9d89f4704b2e457f86a96
Stepan Maznyi <smazni@gmail.com>
2024-09-08 21:25:08 +0300

    Rewrite table class

--- lib/Game/Table/Interfaces.cs
+++
@@ -1,9 +0,0 @@
-namespace PaintedPoker.Game.Table;
-
-public interface IReadOnlyRow<TCell> {
-    TCell this[int index] { get; }
-}
-
-public interface IRow<TCell> {
-    TCell this[int index] { get; set; }
-}
--- lib/Game/Table/Table.cs
+++ lib/Game/Table/Table.cs
@@ -1,19 +1,68 @@
+using System.Diagnostics;
+
 namespace PaintedPoker.Game.Table;
 
-public class Table<TCell>
+public interface IRow<TCell>
 {
-    private int _size;
-
-    public Table(int size)
+    int Count { get; }
+    TCell this[int index]
     {
-        _size = size;
+        get; set;
+    }
+}
+
+public abstract class Table<TCell, TRow> where TRow : IRow<TCell>
+{
+    public abstract TRow NewRow();
+    public abstract int ColumnCount { get; }
+    public virtual IList<TRow> Rows { get; set; } = new List<TRow>();
+}
+
+public abstract class TableWithHeader<TCell, TRow, THeader> : Table<TCell, TRow> where TRow : IRow<TCell>
+{
+    public TableWithHeader(IEnumerable<THeader> headers)
+    {
+        Headers = headers.ToList();
     }
 
-    public List<Row<TCell>> Rows { get; set; } = new List<Row<TCell>>();
-    public virtual int ColumnCount { get => _size; }
+    public IReadOnlyList<THeader> Headers { get; set; }
+}
 
-    public Row<TCell> NewRow()
+public class GameTable<TCell, TPlayer, TRowHeader> : TableWithHeader<TCell, RowWithHeader<TCell, TRowHeader>, TPlayer>
+{
+    public GameTable(IEnumerable<TPlayer> headers) : base(headers) { }
+
+    public override int ColumnCount => Headers.Count;
+
+    public override RowWithHeader<TCell, TRowHeader> NewRow()
     {
-        return new Row<TCell>(ColumnCount);
+        return new RowWithHeader<TCell, TRowHeader>(ColumnCount);
     }
+}
+
+public class Row<TCell> : IRow<TCell>
+{
+    private TCell[] _data;
+
+    internal Row(int size)
+    {
+        Count = size;
+        _data = new TCell[size];
+    }
+
+    public TCell this[int index]
+    {
+        get => _data[index];
+        set => _data[index] = value;
+    }
+
+    public int Count { get; init; }
+}
+
+public class RowWithHeader<TCell, THeader> : Row<TCell>
+{
+    internal RowWithHeader(int size) : base(size) { }
+
+#nullable disable
+    public THeader Header { get; set; }
 }
