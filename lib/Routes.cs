bacadfaeee6fcd2fc518dd901b743b1e81abf27e
Stepan Maznyi <smazni@gmail.com>
2024-09-08 21:26:10 +0300

    feat: add nav url

--- lib/Routes.cs
+++ lib/Routes.cs
@@ -1,6 +1,8 @@
 using PaintedPoker.Game.Services;
 
-public static class Nav {
+public static class Nav
+{
+    public const string CreateGame = "createGame";
     public const string Games = "games";
     public static string Game(GameName gameName) => string.Concat(Games, "/", gameName.name);
 }
