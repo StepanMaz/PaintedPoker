64e903a5e3390a25d6c95331f1e1ce92c6d42b39
Stepan Maznyi <smazni@gmail.com>
2024-11-22 17:41:28 +0200

    feat: add contains key

--- App/lib/PlayerCollection.cs
+++ App/lib/PlayerCollection.cs
@@ -9,13 +9,19 @@ public class PlayerCollection<TValue> : Dictionary<Guid, TValue>
         get => this[key.Id];
         set => this[key.Id] = value;
     }
- 
+
     public void Add(Player player, TValue value)
     {
         Add(player.Id, value);
     }
 
-    public bool TryGetValue(Player player, out TValue value) {
+    public bool TryGetValue(Player player, out TValue value)
+    {
         return TryGetValue(player.Id, out value!);
+    }
+
+    public bool ContainsKey(Player player)
+    {
+        return ContainsKey(player.Id);
     }
 }
